becc16bcb168a58f9e666d8365e20872a56c951e
Gordon-Magill <gordon.magill@gmail.com>
2022-11-05 14:42:46 -0700

    Prototype post creation route done, need to alter views to use this

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,11 +1,39 @@
 const router = require('express').Router();
+const { NOW } = require('sequelize');
 const Post = require('../../models/Post')
+const User = require('../../models/User')
+const moment = require('moment')
+
 
 router.get('/', async (req,res) => [
     // Get all posts
 ])
 
 // Create new post
+router.post('/post/create', async (req,res) => {
+    try {
+        let writingUser = await User.findOne({
+            where: {
+                username: req.session.user_id
+            }
+        })
+    
+        writingUser = writingUser.get({plain: true})
+        console.log('writingUser: ',writingUser)
+    
+        const newPost = await Post.create({
+            author_id: writingUser.id,
+            creation_time: Date.now(),
+            content: req.body.postContent
+        })
+    
+        res.status(200).json(newPost)
+    } catch (err) {
+        console.log(err)
+        res.status(400).json(err)
+    }
+    
+})
 
 // Get all posts
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,36 @@
 const router = require('express').Router();
+const Post = require('../models/Post');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
 router.get('/', async (req,res) => {
+    const allPosts = await Post.findAll()
     res.render('homepage', {
+        allPosts,
         sess: req.session,
     })
 })
 
 router.get('/dashboard', async (req,res) => {
-    res.render('dashboard', {})
+    res.render('dashboard', {
+        sess: req.session,
+    })
 })
 
 router.get('/loginSignUp', async (req,res) => {
-    res.render('loginSignUp', {})
+    // If the user is already logged in, just dump them back to the home page
+    if (req.session.logged_in) {
+        res.render('homepage',{
+            sess: req.session
+        })
+    } else {
+        // If user isn't logged in, send them to the login/signup page
+        res.render('loginSignUp', {
+            sess: req.session,
+        })
+    }
+
 })
 
 module.exports = router;
